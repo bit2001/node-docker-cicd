af91948f3d7317beb3e6ab2e03f6c1d77124cd54
bit2001 <32589800+bit2001@users.noreply.github.com>
2026-05-01 16:56:02 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello GitHub Actions + Docker + VM Deploy!");
+  res.send("Hello GitHub Actions + Docker + VM Deploy! v2 ");
 });
 
 app.listen(3000, () => {
